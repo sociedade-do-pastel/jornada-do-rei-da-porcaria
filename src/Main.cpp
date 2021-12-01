e0db0d0a3f88ee93a94dc297ed6884962daa2e6c
guior <guilhermeormond@outlook.com>
2021-11-30 21:05:37 -0300

    Corrigido bug de abertura por comando sem parametro.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,14 @@
 #include "../include/Game.hpp"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    Game game(atoi(argv[1]));
+    int a;
+    if (argc == 1)
+        a = 1;
+    else
+        a = atoi(argv[1]);
+
+    Game game(a);
     bool success = game.initialize();
 
     if (success)
