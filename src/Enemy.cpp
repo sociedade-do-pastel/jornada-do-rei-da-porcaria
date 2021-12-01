ee1cbcd7900c2622f915e3635d7676cb3b669590
guior <guilhermeormond@outlook.com>
2021-11-30 21:13:45 -0300

    Corrigido bug de enemy fora do grid.

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,5 +1,6 @@
 #include "../include/Enemy.hpp"
 #include "../include/Game.hpp"
+#include "../include/World.hpp"
 
 Enemy::Enemy(Game* game) : Actor(game)
 {
@@ -20,6 +21,17 @@ void Enemy::updateActor()
             setSpeed(100);
         }
     }
+
+    auto a = getGame()->getWorld()->get_top_left();
+
+    if (getPosition().x < a.x)
+        setPosition({a.x + 25, getPosition().y});
+    if (getPosition().y < a.y)
+        setPosition({getPosition().x, a.y + 25});
+    if (getPosition().x > a.x + 768)
+        setPosition({a.x + 743, getPosition().y});
+    if (getPosition().y > a.y + 768)
+        setPosition({getPosition().x, a.y + 743});
 
     if (getHP() <= 0)
         setState(Actor::State::Dead);
